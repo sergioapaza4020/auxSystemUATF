032c973bf24b8c2cf66eb5fdbd63ab3e47518362
Zergio23 <sergio.apaza1432@gmail.com>
2026-03-30 18:23:34 -0400

    fix: eslint fix in backend

--- apps/backend-nest/src/main.ts
+++ apps/backend-nest/src/main.ts
@@ -16,4 +16,7 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3001);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Error during bootstrap:', err);
+  process.exit(1);
+});
