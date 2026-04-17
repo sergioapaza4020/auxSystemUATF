13ae3bc14e10f157294de5a578ce8aca1e8f8c08
Zergio23 <sergio.apaza1432@gmail.com>
2026-04-16 21:39:10 -0400

    fix(test): fix tests in backend mocking data

--- apps/backend-nest/src/califications/services/users/users.service.spec.ts
+++ apps/backend-nest/src/califications/services/users/users.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/califications/entities/users/users.entity';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const mockUserRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: getRepositoryToken(User), useValue: mockUserRepository },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
